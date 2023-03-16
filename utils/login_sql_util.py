1b83831cd5fa817abe0b8820d8f03b5c2fbdb141
ruancx <2975701566@qq.com>
2023-03-16 16:55:25 +0800

    增加用户登录/注册，并获取用户的token

--- utils/login_sql_util.py
+++ utils/login_sql_util.py
@@ -2,23 +2,13 @@ import pymysql
 import pytest
 import requests
 
-# class LoginYzmSql():
-#
-#     def test_yzm(self):
-#         # 路径，获取yaml配置文件中设置的url
-#         url = 'https://b2b2c-api-test.mall4j.com/sms/sendLoginCode'
-#         # 需要传入的数据,获取yaml配置文件中设置的参数
-#         data = {"mobile": "18144632758"}
-#         # 发送post请求
-#         res = requests.post(url, json=data).json()
-#         print(res)
 
-# 获取用户发送的验证码
+# 发送并获取验证码
 def get_data():
 
-    # 路径，获取yaml配置文件中设置的url
+    # 发送验证码的接口路径
     url = 'https://b2b2c-api-test.mall4j.com/sms/sendLoginCode'
-    # 需要传入的数据,获取yaml配置文件中设置的参数
+    # 参数
     data = {"mobile": "17111111111"}
     # 发送post请求
     res = requests.post(url, json=data).json()
@@ -32,14 +22,14 @@ def get_data():
         port=30750,
         db='mall4j_b2b2c_test',
         )
-
     # 创建游标对象
     cursor = db.cursor()
-
+    # sql语句
     sql = 'SELECT mobile_code FROM tz_sms_log WHERE user_phone = 17111111111 AND `type` = 13 ORDER BY rec_date DESC LIMIT 1'
-
+    # 执行sql语句
     cursor.execute(sql)
     data = cursor.fetchone()
+    # 关闭数据库
     db.close()
     print(data)
     return data
